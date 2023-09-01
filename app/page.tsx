c35a5658e3bdded27423453881bcf1b74eda4ee8
Rianfirmansyah071020 <rian071020@gmail.com>
2023-09-01 09:00:29 +0700

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <p className="text-3xl font-boldtext-indigo-500">Hello Discord Clone</p>
-      <Button variant="ghost">Click Me</Button>
+      <Button>Click Me</Button>
     </div>
   );
 }

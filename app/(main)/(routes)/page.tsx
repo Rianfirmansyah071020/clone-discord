4e3a61948b60870cdd1ad613121dfda764c0e650
Rianfirmansyah071020 <rian071020@gmail.com>
2023-09-01 15:11:27 +0700

    dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,8 @@
+import { UserButton } from "@clerk/nextjs/app-beta/client";
 export default function Home() {
-  return <div>This Protected Route.</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
